84cf1f4e8fa7744b7dd1f258a874630579049870
Jonathan Baxter <jdbaxte3@ncsu.edu>
2025-03-22 21:37:18 -0400

    added player rotation

--- main.asm
+++ main.asm
@@ -18,28 +18,82 @@ handle_keyboard:
 	mov word [player_speed_y], 0
 	in al, 0x60
 	cmp al, 0x11
+	mov bl, al
 	jne .skip_w
-	dec word [player_speed_y]
+	mov si, sin_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	add [player_speed_x], ax
+	mov si, cos_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	add [player_speed_y], ax
+	mov al, bl
 .skip_w:
 	cmp al, 0x1E
+	mov bl, al
 	jne .skip_a
-	dec word [player_speed_x]
+	mov si, sin_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	sub [player_speed_y], ax
+	mov si, cos_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	add [player_speed_x], ax
+	mov al, bl
 .skip_a:
+	mov bl, al
 	cmp al, 0x1F
 	jne .skip_s
-	inc word [player_speed_y]
+	mov si, sin_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	sub [player_speed_x], ax
+	mov si, cos_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	sub [player_speed_y], ax
+
 .skip_s:
 	cmp al, 0x20
+	mov bl, al
 	jne .skip_d
-	inc word [player_speed_x]
+	mov si, sin_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	add [player_speed_y], ax
+	mov si, cos_table
+	add si, [player_angle_int]
+	mov al, [si]
+	cbw
+	sub [player_speed_x], ax
+	mov al, bl
 .skip_d:
+	cmp al, 0x4B
+	jne .skip_left_arrow
+	inc word [player_angle_int]
+	and word [player_angle_int], 32-1
+.skip_left_arrow:
+	cmp al, 0x4D
+	jne .skip_right_arrow
+	dec word [player_angle_int]
+	and word [player_angle_int], 32-1
+.skip_right_arrow:
 
 	; Move player
 	mov ax, [player_x]
 	add ax, [player_speed_x]
-	shr ax, 3
+	shr ax, 10
 	mov bx, [player_y]
-	shr bx, 3
+	shr bx, 10
 	shl bx, 3
 	add ax, bx
 	mov si, map
@@ -52,10 +106,10 @@ handle_keyboard:
 .skip_player_move_x:
 	mov ax, [player_y]
 	add ax, [player_speed_y]
-	shr ax, 3
+	shr ax, 10
 	shl ax, 3
 	mov bx, [player_x]
-	shr bx, 3
+	shr bx, 10
 	add ax, bx
 	mov si, map
 	add si, ax
@@ -88,6 +142,8 @@ wait_frame:
 draw_player:
 	mov bx, [player_x]
 	mov ax, [player_y]
+	shr ax, 7
+	shr bx, 7
 	mov cx, 320
 	mul word cx
 	add ax, bx
@@ -127,6 +183,14 @@ draw_map:
 	ret
 .i: dw 0
 
+draw_line:
+	; cx = height
+	; di = x coordinate
+	mov al, 0x20
+	stosb
+	add di, 320
+	ret
+
 draw_sprite8:
 	; si = sprite pointer
 	; bx = x coordinate
@@ -146,8 +210,9 @@ draw_sprite8:
 	ret
 
 ; Data
-player_x: dw 12
-player_y: dw 12
+player_x: dw 12*128
+player_y: dw 12*128
+player_angle_int: dw 0
 player_speed_x: dw 0
 player_speed_y: dw 0
 
@@ -167,11 +232,23 @@ map:
 	db 1,1,1,1,1,1,1,1
 	db 1,0,1,0,0,0,0,1
 	db 1,0,1,0,0,1,1,1
-	db 1,0,1,0,0,0,0,1
-	db 1,0,1,0,0,0,0,1
 	db 1,0,0,0,0,0,0,1
-	db 1,0,0,0,0,0,0,1
+	db 1,0,0,1,0,0,0,1
+	db 1,0,1,0,0,0,0,1
+	db 1,1,1,0,0,0,0,1
 	db 1,1,1,1,1,1,1,1
+
+sin_table:
+	db 0,24,48,70,90,106,117,125
+	db 127,125,117,106,90,70,48,24
+	db 0,-25,-49,-71,-91,-107,-118,-126
+	db -128,-126,-118,-107,-91,-71,-49,-25
+
+cos_table:
+	db 127,125,117,106,90,70,48,24
+	db 0,-25,-49,-71,-91,-107,-118,-126
+	db -128,-126,-118,-107,-91,-71,-49,-25
+	db -1,24,48,70,90,106,117,125
 
 
 times 1024-($-$$) db 0
